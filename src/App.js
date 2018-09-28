cc89d91e45803a25f662c95657ee739c9578a797
samsdial <diastudios@gmail.com>
2018-09-28 15:21:34 -0500

    Add Frist --

--- src/App.js
+++ src/App.js
@@ -1,54 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './PHP-logo.png';
+import logoOPT from './logo_optime.svg';
 import './App.css';
-/*Classic Function*/
-/*function Hello (props) {
-    return <h2>{props.title}</h2>
-}*/
-/*Arrow functions sintaxis */
-//const Hello = (props) => <h2>{props.title}</h2>
-/*ECMas 13*/
-class Hello extends Component{
-    render (){
-        return <h2>{this.props.title}</h2>
-    }
-}
-class Text extends Component {
-    render () {
-        const {
-            arrayOfNumbers,
-            multiply,
-            objectWithInfo,
-            title
-        } = this.props;
-        const mappedNumbers = arrayOfNumbers.map(n => n * 2);
-        return (
-            <div>
-                {title}
-                <p>{mappedNumbers.join(', ')}</p>
-                <p>{this.props.objectWithInfo.key}</p>
-            </div>
-            );
-    }
-}
 class App extends Component {
   render() {
       return (
           <div className="App">
               <header className="App-header">
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <Hello title="Hola Optime"/>
+                  <div className="aligner_item">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2> Bienvenidos a el curso de PHP 3.0</h2>
+                    <h2> El Futuro es Ahora</h2>
+                  </div>
               </header>
               <section>
-                  <Text
-                      arrayOfNumbers={[2,3,10]}
-                      objectWithInfo={{ key: 'FisrtValue ', key2: 'otherValue'}}
-                      multiply={(number) => number * 4}
-                      number={2}
-                      text='Esto es PHP 1.1.0'
-                      title={ <h1> Nuestro Titulo </h1> }
-                  />
+                  <p></p>
               </section>
+              <footer className="App-footer">
+                  <div className="aligner_item">
+                      <h3> Sergio A. Martinez</h3>
+                      <img src={logoOPT} className="App-logo" alt="logo" />
+                  </div>
+              </footer>
           </div>
       );
 
