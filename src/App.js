e3dee65a0e36dd84d29bc491e75f4ca845ee1d25
samsdial <diastudios@gmail.com>
2018-09-28 16:20:00 -0500

    Add  Componentes

--- src/App.js
+++ src/App.js
@@ -13,27 +13,12 @@ class App extends Component {
                   <div className="aligner_item">
                     <img src={logo} className="App-logo App-logo-react" alt="logo" />
                     <h2> Bienvenidos a el curso de Reactjs</h2>
-                    <h2> Estamos en el Futuro</h2>
+                    <h2> Componentes</h2>
                   </div>
               </header>
               <section className="content">
                   <div className="aligner_item">
-                      <h3>¿Qué es React?</h3>
-                      <h4>
-                          Introduccion a jsk
-                      </h4>
-                      <a href="https://babeljs.io/repl/"> BABLE repl</a>
-                      <p>
-                          <br/>
-                          Origen Facebook
-                          Liberada en GitBut 2013
-                      </p>
 
-                      <pre class="pre1">
-                          const element = &lt;h1&gt;hello, world&lt;/h1&gt;; <br/>
-                          const image = &lt;img src= /img&gt;; <br/>
-
-                        </pre>
                   </div>
               </section>
               <footer className="App-footer">
