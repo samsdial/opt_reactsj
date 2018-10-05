e526c856e2db35caf8d1d5fb9f757e7486fd9ca8
samsdial <diastudios@gmail.com>
2018-10-05 11:50:49 -0500

    Add Inicio Curso React ONE add Que es React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './PHP-logo.png';
+import logo from './logo.svg';
 import logoOPT from './logo_optime.svg';
 import './App.css';
 class App extends Component {
@@ -8,18 +8,27 @@ class App extends Component {
           <div className="App">
               <header className="App-header">
                   <div className="aligner_item">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2> Bienvenidos a el curso de PHP 3.0</h2>
-                    <h2> El Futuro es Ahora</h2>
+                    <img src={logo} className="App-logo App-logo-react" alt="logo" />
+                    <h2> Bienvenidos a el curso de Reactjs</h2>
+                    <h2> Estamos en el Futuro</h2>
                   </div>
               </header>
-              <section>
-                  <p></p>
+              <section className="content">
+                  <div className="aligner_item">
+                      <h3>¿Qué es React?</h3>
+                      <ul className="text_left">
+                          <li>Conceptos Básicos</li>
+                          <li>Introducción A JSK</li>
+                          <li>Componentes React JS</li>
+                          <li>Props</li>
+                          <li>State</li>
+                      </ul>
+                  </div>
               </section>
               <footer className="App-footer">
                   <div className="aligner_item">
                       <h3> Sergio A. Martinez</h3>
-                      <img src={logoOPT} className="App-logo" alt="logo" />
+                      <img src={logoOPT} className="App-logo App-logo-opt" alt="logo" />
                   </div>
               </footer>
           </div>
